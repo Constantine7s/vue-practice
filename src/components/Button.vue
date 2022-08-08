7f69152ca77924ae4406159ec1f6669366198ce7
Konstantin Shambazov <k.shambazov@gmail.com>
2022-08-08 10:55:21 +0900

    feat: add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <button class="btn">Create Task</button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+}
+</script>
